d00b8a0f889a912667f4504f0fc45051a1748d9b
Monica Hong <momo.star@gmail.com>
2020-07-25 02:02:50 -0700

    merchant ship

--- Models/Cards/MerchantShip/MerchantShip.cs
+++ Models/Cards/MerchantShip/MerchantShip.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace LootGame.Models.Cards
 {
     [NotMapped]
-    public class MerchantShip
+    public class MerchantShip : Card
     {
-
+        public MerchantShip(string type = "MerchantShip", string color = "", int atkStr = 0, int gValue = 0) : base(type, color, atkStr, gValue) { }
     }
 }
