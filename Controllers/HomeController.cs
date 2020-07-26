204f2d031416df8f91a302764075ea498908d192
Monica Hong <momo.star@gmail.com>
2020-07-25 23:20:11 -0700

    start screen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,9 +18,10 @@ namespace LootGame.Controllers
             _logger = logger;
         }
 
+        [HttpGet("/")]
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
